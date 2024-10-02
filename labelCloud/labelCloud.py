0255a6946dc26c99f72f526139013f6148f3ae41
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-10-02 16:16:56 +0800

    Update labelCloud.py

--- labelCloud/labelCloud.py
+++ labelCloud/labelCloud.py
@@ -1,5 +1,5 @@
 from labelCloud.__main__ import main
 import logging
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log', filemode='w')
+    #logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log', filemode='w')
     main()
