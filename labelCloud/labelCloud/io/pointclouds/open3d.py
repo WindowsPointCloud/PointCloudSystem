c8fb7df2fa9c0ae8a0022c6a1d052d17bd6eae4e
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-10-10 15:10:42 +0800

    Update open3d.py

--- labelCloud/labelCloud/io/pointclouds/open3d.py
+++ labelCloud/labelCloud/io/pointclouds/open3d.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Optional, Tuple
 
 import numpy as np
-import numpy.typing as npt
+#import numpy.typing as npt
 import open3d as o3d
 
 from . import BasePointCloudHandler
@@ -20,7 +20,7 @@ class Open3DHandler(BasePointCloudHandler):
     @staticmethod
     def to_point_cloud(
         pointcloud: o3d.geometry.PointCloud,
-    ) -> Tuple[npt.NDArray, Optional[npt.NDArray]]:
+    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
         return (
             np.asarray(pointcloud.points).astype("float32"),
             np.asarray(pointcloud.colors).astype("float32"),
@@ -34,7 +34,7 @@ class Open3DHandler(BasePointCloudHandler):
         o3d_pointcloud.colors = o3d.utility.Vector3dVector(pointcloud.colors)
         return o3d_pointcloud
 
-    def read_point_cloud(self, path: Path) -> Tuple[npt.NDArray, Optional[npt.NDArray]]:
+    def read_point_cloud(self, path: Path) -> Tuple[np.ndarray, Optional[np.ndarray]]:
         super().read_point_cloud(path)
         return self.to_point_cloud(
             o3d.io.read_point_cloud(str(path), remove_nan_points=True)
