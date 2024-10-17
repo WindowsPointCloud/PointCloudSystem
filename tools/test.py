8f30b29aa1589319a1ed7e5c6f8e8d91dcd7d393
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-10-17 16:47:11 +0800

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -63,6 +63,9 @@ def eval_single_ckpt(model, test_loader, args, eval_output_dir, logger, epoch_id
         cfg, model, test_loader, epoch_id, logger, dist_test=dist_test,
         result_dir=eval_output_dir, save_to_file=args.save_to_file
     )
+    
+   
+   
 
 
 def get_no_evaluated_ckpt(ckpt_dir, ckpt_record_file, args):
