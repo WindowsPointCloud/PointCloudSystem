2b3c7f40ea70ea8de2789123301c4e70d354c8fe
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2024-11-04 22:22:40 +0800

    Update eval_utils.py

--- tools/eval_utils/eval_utils.py
+++ tools/eval_utils/eval_utils.py
@@ -4,9 +4,17 @@ import time
 import numpy as np
 import torch
 import tqdm
+import yaml
 
 from pcdet.models import load_data_to_gpu
 from pcdet.utils import common_utils
+
+
+def get_key_by_value(d, target_value):
+    for key, value in d.items():
+        if value == target_value:
+            return key
+    return None  # Return None if the value is not found
 
 
 def statistics_info(cfg, ret_dict, metric, disp_dict):
@@ -19,11 +27,11 @@ def statistics_info(cfg, ret_dict, metric, disp_dict):
         '(%d, %d) / %d' % (metric['recall_roi_%s' % str(min_thresh)], metric['recall_rcnn_%s' % str(min_thresh)], metric['gt_num'])
 
 
-def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, save_to_file=False, result_dir=None):
+def eval_one_epoch(cfg, args, model, dataloader, epoch_id, logger, dist_test=False, result_dir=None):
     result_dir.mkdir(parents=True, exist_ok=True)
 
     final_output_dir = result_dir / 'final_result' / 'data'
-    if save_to_file:
+    if args.save_to_file:
         final_output_dir.mkdir(parents=True, exist_ok=True)
 
     metric = {
@@ -36,6 +44,10 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
     dataset = dataloader.dataset
     class_names = dataset.class_names
     det_annos = []
+
+    if getattr(args, 'infer_time', False):
+        start_iter = int(len(dataloader) * 0.1)
+        infer_time_meter = common_utils.AverageMeter()
 
     logger.info('*************** EPOCH %s EVALUATION *****************' % epoch_id)
     if dist_test:
@@ -53,15 +65,25 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
     start_time = time.time()
     for i, batch_dict in enumerate(dataloader):
         load_data_to_gpu(batch_dict)
-        
+
+        if getattr(args, 'infer_time', False):
+            start_time = time.time()
+
         with torch.no_grad():
             pred_dicts, ret_dict = model(batch_dict)
+
         disp_dict = {}
+
+        if getattr(args, 'infer_time', False):
+            inference_time = time.time() - start_time
+            infer_time_meter.update(inference_time * 1000)
+            # use ms to measure inference time
+            disp_dict['infer_time'] = f'{infer_time_meter.val:.2f}({infer_time_meter.avg:.2f})'
 
         statistics_info(cfg, ret_dict, metric, disp_dict)
         annos = dataset.generate_prediction_dicts(
             batch_dict, pred_dicts, class_names,
-            output_path=final_output_dir if save_to_file else None
+            output_path=final_output_dir if args.save_to_file else None
         )
         det_annos += annos
         if cfg.LOCAL_RANK == 0:
@@ -91,7 +113,7 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
         metric = metric[0]
 
     gt_num_cnt = metric['gt_num']
-    for cur_thresh in cfg.MODEL.POST_PROCESSING.RECALL_THRESH_LIST:
+    for cur_thresh in [0.5]: # cfg.MODEL.POST_PROCESSING.RECALL_THRESH_LIST:
         cur_roi_recall = metric['recall_roi_%s' % str(cur_thresh)] / max(gt_num_cnt, 1)
         cur_rcnn_recall = metric['recall_rcnn_%s' % str(cur_thresh)] / max(gt_num_cnt, 1)
         logger.info('recall_roi_%s: %f' % (cur_thresh, cur_roi_recall))
@@ -101,15 +123,12 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
 
     total_pred_objects = 0
     for anno in det_annos:
-        
-  
         total_pred_objects += anno['name'].__len__()
     logger.info('Average predicted number of objects(%d samples): %.3f'
                 % (len(det_annos), total_pred_objects / max(1, len(det_annos))))
 
     with open(result_dir / 'result.pkl', 'wb') as f:
         pickle.dump(det_annos, f)
-    
 
     result_str, result_dict = dataset.evaluation(
         det_annos, class_names,
@@ -117,10 +136,46 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
         output_path=final_output_dir
     )
 
-    logger.info(result_str)
+    with open(cfg.DATA_CONFIG._BASE_CONFIG_, 'r') as file:
+         data = yaml.safe_load(file)
+    MAP_CLASS_TO_KITTI = data['MAP_CLASS_TO_KITTI']
+    print()
+
+    data_list = result_str.split('\n')
+
+    # metrics
+    group_size = 5
+    groups_per_class = 4
+
+    # Split data_list into groups of 5 strings
+    groups = [data_list[i:i + group_size] for i in range(0, len(data_list), group_size)]
+
+    # Split groups into classes of 4 groups each
+    classes = [groups[i:i + groups_per_class] for i in range(0, len(groups), groups_per_class)]
+
+    # Loop through each class and process the groups
+    for class_group in classes:
+        for group in class_group:
+            first_string = group[0]  # Access the first string in the group
+            # Check conditions in the first string
+            if "AP_R40" in first_string and "0.50" in first_string and "0.75" not in first_string:
+                #print(group)  # Print the group
+                value = cls = group[0].split()[0]
+                key = get_key_by_value(MAP_CLASS_TO_KITTI, value)
+                #print(key, value)
+                print(group[0].replace(value, key))
+                print(group[1])
+                print()
+                break  # Skip to the next class
+
+    print('recall_roi_%s: %f' % (cur_thresh, cur_roi_recall))
+    print('recall_rcnn_%s: %f' % (cur_thresh, cur_rcnn_recall))
+    print()
+
+    #logger.info(result_str)
     ret_dict.update(result_dict)
 
-    logger.info('Result is save to %s' % result_dir)
+    logger.info('Result is saved to %s' % result_dir)
     logger.info('****************Evaluation done.*****************')
     return ret_dict
 
