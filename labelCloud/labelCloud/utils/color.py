2b86b0598ab29b5e8a3ae14541e277c6e24c6b46
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-12-03 20:20:02 +0800

    Update color.py

--- labelCloud/labelCloud/utils/color.py
+++ labelCloud/labelCloud/utils/color.py
@@ -4,7 +4,8 @@ from nptyping import NDArray, Float32, Shape
 import numpy as np
 import numpy.typing as npt
 import pkg_resources
-
+import sys 
+from pathlib import Path
 from ..definitions.types import Color3f
 
 
@@ -35,8 +36,11 @@ def get_distinct_colors(n: int) -> List[str]:
 def colorize_points_with_height(
     points: NDArray[Any, Float32], z_min: float, z_max: float
 ) -> NDArray[Any, Float32]:
+  #  palette = np.loadtxt(
+  #      pkg_resources.resource_filename("labelCloud.resources", "rocket-palette.txt")
+  #  )
     palette = np.loadtxt(
-        pkg_resources.resource_filename("labelCloud.resources", "rocket-palette.txt")
+    get_palette_path("rocket-palette.txt")
     )
     palette_len = len(palette) - 1
 
@@ -45,6 +49,16 @@ def colorize_points_with_height(
         colors[ind] = palette[round((height - z_min) / (z_max - z_min) * palette_len)]
     return colors.astype(np.float32)
 
+def get_palette_path(filename):
+    """Get the path to the icon file, considering whether running in PyInstaller bundle or not."""
+    if getattr(sys, 'frozen', False):
+        # Running in a PyInstaller bundle
+        base_path = Path(sys._MEIPASS)
+    else:
+        # Running in a development environment
+        base_path = Path(__file__).resolve().parent.parent.parent
+    
+    return base_path / "resources" / filename
 
 def hex_to_rgb(hex: str) -> Color3f:
     """Converts a hex color to a list of RGBA values.
